92a95af8da814b6615e0e5c59b7f93812181a6c5
fedeyak <fedeyak@gmail.com>
2025-04-26 12:20:54 +0300

    fix typos

--- src/main/java/codereview/school_mate/repository/StudentRepository.java
+++ src/main/java/codereview/school_mate/repository/StudentRepository.java
@@ -1,13 +1,12 @@
 package codereview.school_mate.repository;
 
-<<<<<<< HEAD
-public interface StudentRepository {
-=======
+
 import codereview.school_mate.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
->>>>>>> pr-3
+
 }
